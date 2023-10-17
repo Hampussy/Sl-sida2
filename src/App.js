394fe21db74fb91af3699c720502708159a237f8
SpaxTVS <112695925+SpaxTVS@users.noreply.github.com>
2023-10-17 11:28:58 +0200

    ganger

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-
+apikey = 'a9255bbb03194d14a7186bcf6f14558b'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='destination'></div>
+      <div className='displaytime'></div>
+      <div className='linenumber'></div>
     </div>
   );
 }

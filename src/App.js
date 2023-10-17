c2707adf1b38cb7fd1581bc22843dddfcc525398
Hampussy <hampuskungsman@gmail.com>
2023-10-17 11:30:02 +0200

    Fungerande apiKey

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 
-
-apikey = 'a9255bbb03194d14a7186bcf6f14558b'
+apiUrl = 'https://api.sl.se/api2/realtimedeparturesV4.Json?key=23997f00503e4100862f3dce6ca22690&siteid=7044&timewindow=30&Bus=true'
 
 function App() {
   return (

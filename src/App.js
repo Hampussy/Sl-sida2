c8ef8be5eac4ca69015eb1a8a69ed8d47fc63374
Hampussy <hampuskungsman@gmail.com>
2023-10-20 08:46:07 +0200

    Axios

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import axios from axios
 apikey = 'a9255bbb03194d14a7186bcf6f14558b'
 
 apiUrl = 'https://api.sl.se/api2/realtimedeparturesV4.Json?key=23997f00503e4100862f3dce6ca22690&siteid=7044&timewindow=30&Bus=true'

b732aae5c8cbc2e24b00827e714f55f259ef8df8
Hampussy <hampuskungsman@gmail.com>
2023-11-10 10:17:20 +0100

    Penis

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
-import axios from axios
-apikey = 'a9255bbb03194d14a7186bcf6f14558b'
+import axios from Axios
+import { useState } from 'react';
+import { Axios } from 'axios';
+const apikey = 'a9255bbb03194d14a7186bcf6f14558b';
 
-apiUrl = 'https://api.sl.se/api2/realtimedeparturesV4.Json?key=23997f00503e4100862f3dce6ca22690&siteid=7044&timewindow=30&Bus=true'
+const apiPlats = '6d89ea803f5d43a8893f8ea50a03ef52';
+
+const platsKollYani = 'https://api.sl.se/api2/typeahead.json?searchstring=Huddinge&stationsonly=true&maxresults=10&key=;6d89ea803f5d43a8893f8ea50a03ef52'
+
+const apiUrl = 'https://api.sl.se/api2/realtimedeparturesV4.Json?key=23997f00503e4100862f3dce6ca22690&siteid=7000&timewindow=30&Bus=true';
+
+const [bussData, setBussData] = useState ({});
+
+const searchData = event => {
+  if (event.onload)
+}
+
 
 function App() {
   return (
     <div className="App">
-      <div className='destination'></div>
+      <div className='destination'>
+        <p>{Bussar[0].Destination}</p>
+      </div>
       <div className='displaytime'></div>
       <div className='linenumber'></div>
     </div>
